3e9e5bf46ada671787691bf51797b679928029b0
Tian <lt22@mails.tsinghua.edu.cn>
2022-09-30 11:59:07 +0800

    add seaborn practice

--- src/hw1/new_utils.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-
-df = pd.read_csv("../../data/OnlineNewsPopularity/OnlineNewsPopularity.csv")
-df.drop(labels=['url', ' timedelta'], axis = 1, inplace=True)
-
-df.describe()
-print(df.describe())
