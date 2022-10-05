dce02687d70bfafec79a0cca184797bb9e6438be
Tian <lt22@mails.tsinghua.edu.cn>
2022-10-05 16:34:54 +0800

    Add methods to calculate the metrics

--- src/hw1/predict_decision_tree.py
+++ src/hw1/predict_decision_tree.py
@@ -1,3 +1,4 @@
+import re
 import numpy as np
 import pandas as pd
 from models.decision_tree import DecisionTreeClassifier
@@ -21,8 +22,19 @@ def get_split(df_d, proportion=0.8):
 train_set, train_label, test_set, test_label = get_split(df, proportion=0.8)
 
 clf = DecisionTreeClassifier(criterion="entropy").fit(train_set, train_label)
+result = clf.predict(test_set)
 score = clf.score(test_set, test_label) 
-print(score)
 
+from sklearn.metrics import roc_auc_score
+from sklearn.metrics import plot_roc_curve, f1_score
+import matplotlib.pyplot as plt
+
+# print("here")
+# print(roc_auc_score(test_label, result))
+# plot_roc_curve(clf, test_set, test_label)
+# plt.show()
+
+f1 = f1_score(test_label, result)
+print(f1)
 
 
